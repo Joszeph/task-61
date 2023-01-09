289019ed33f06076b1fa3c175a4dd1ebf5adcace
Yosif Georgiev <iodesign@live.com>
2023-01-09 18:49:39 +0200

    NEW: add fire icon

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const hotPrice = document.querySelector('.hot')
+
+  hotPrice.innerHTML += ' 🔥'
 });
